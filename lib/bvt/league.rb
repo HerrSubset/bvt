1e5366eecaa35c828bee3504d786a9bd04ac9668
HerrSubset <smets.pj@gmail.com>
2016-02-19 18:56:15 +0100

    make it possible to request a teams calendar

--- lib/bvt/league.rb
+++ lib/bvt/league.rb
@@ -17,4 +17,42 @@ class Bvt::League
       @games.push(game)
     end
   end
+
+
+  #return an array listing all the teams in this league
+  def get_teams
+    res = Array.new
+
+    @games.each do |g|
+      #check if home team is in the result array
+      if !res.include?(g.home_team)
+        res.push(g.home_team)
+      end
+
+      #check if away team is in the result array
+      if !res.include?(g.away_team)
+        res.push(g.away_team)
+      end
+    end
+
+    return res
+  end
+
+
+  #return an array with all the games belonging to a certain team
+  def get_team_games(team)
+    res = Array.new
+
+    @games.each do |g|
+      if team == g.home_team
+        res.push(g)
+      end
+
+      if team == g.away_team
+        res.push(g)
+      end
+    end
+
+    return res
+  end
 end
